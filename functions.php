2e074283ae98d8f3049996eb2e135b5c25d04b4d
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-12-22 22:39:08 +0600

    Enqueueing js file

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
 <?php 
 
-// link to stylesheet
+// Enqueueing styles and scripts
 function themeDev_enqueueing() {
+    // link to stylesheet
     wp_enqueue_style( 'main-stylesheet', get_stylesheet_uri(), array(), filemtime(get_template_directory().'/style.css'), 'all');
+    // link to javascript file
+    wp_enqueue_script( 'main-javascript', get_template_directory_uri() . '/index.js', array('jquery'), filemtime(get_template_directory().'/index.js'), true );
+    // link to Google Fonts
+    wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap', array(), null, 'all');
 }
 add_action( 'wp_enqueue_scripts', 'themeDev_enqueueing' );
