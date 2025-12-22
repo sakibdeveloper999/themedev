8f2ad78f4cd29ca83423d4d7a10369545ff8ea46
sakibdeveloper999 <arjunsakib43@gmail.com>
2025-12-22 21:44:14 +0600

    update stylesheet linking and enhance main content styles in theme

--- functions.php
+++ functions.php
@@ -2,6 +2,6 @@
 
 // link to stylesheet
 function themeDev_enqueueing() {
-    wp_enqueue_style( 'main-stylesheet', get_stylesheet_uri(), array(), '1.0', 'all' );
+    wp_enqueue_style( 'main-stylesheet', get_stylesheet_uri(), array(), filemtime(get_template_directory().'/style.css'), 'all');
 }
 add_action( 'wp_enqueue_scripts', 'themeDev_enqueueing' );
--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php get_header(); ?>
     <main>
+        <div class="main-content">
         <h1 class="main-title">it's main</h1>
+        <p class="main-content-text">This is the main content area.</p>
+        </div>
     </main>
 <?php get_footer(); ?>
