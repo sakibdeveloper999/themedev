d5e28d1f0b78aa269549691c9754e958afb681c3
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-04 19:46:06 +0600

    add the_permalink function

--- page-home.php
+++ page-home.php
@@ -50,8 +50,8 @@
                         ?>
 
                         <article class="latest-news">
-                            <?php the_post_thumbnail('large'); ?>
-                            <h3><?php the_title() ?></h3>
+                            <a href="<?php the_permalink(  ); ?>"><?php the_post_thumbnail('large'); ?></a>
+                            <a href="<?php the_permalink(  ); ?>"><h3><?php the_title() ?></h3></a>
                             <div class="meta-info">
                                 <p> by <span><?php the_author_posts_link(); ?></span> Categories: <span><?php the_category(' '); ?></span> Tags: <?php the_tags( '', ', ', '' ) ?></p>
                                 <p><span><?php echo get_the_date(); ?></span></p>
