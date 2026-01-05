2e2fac72cc9665c81baa12a8ebfd4fd56e529de1
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-05 14:22:56 +0600

    enable the get_search_form

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
                  </div>
 
                 <div class="searchbox">
-                    Search
+                    <?php get_search_form(); ?>
                 </div>
                 </div>
 
