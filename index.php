5d3db58ff424b328c51e88c99ec0e2fc5ef6bcc7
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-06 20:08:53 +0600

    update the content Area

--- index.php
+++ index.php
@@ -15,19 +15,11 @@
                           <?php 
                          if(have_posts(  )):
                             while(have_posts(  )):the_post(  );
-                        ?>
-                        <!-- content -->
-                        <article>
-                            <a href="<?php the_permalink(  ); ?>"><h2><?php the_title() ?></h2></a>
-                            <a href="<?php the_permalink(  ); ?>"><?php the_post_thumbnail(array(275,275)); ?></a>
-                            <div class="meta-info">
-                                <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link( ); ?> </p>
-                                <p>Categories: <?php the_category(', '); ?></p>
-                                <p>Tags: <?php the_tags( '', ', ', '' ) ?></p>
-                            </div>
-                            <?php the_excerpt(); ?>
-                        </article>
-                        <?php endwhile;?>
+
+                            // Content from part/content.php 
+                            get_template_part( 'part/content' );
+                            
+                         endwhile;?>
                         <!--  Pagination -->
                         <div class="wpdevs-pagination">
                          <div class="pages new">
