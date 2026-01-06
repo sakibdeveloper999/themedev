7480f79600944c3bd7bc1b7587ee0c0d313140f3
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-06 20:30:53 +0600

    update page-home.php

--- page-home.php
+++ page-home.php
@@ -47,19 +47,9 @@
 
                          if($postlist -> have_posts(  )):
                             while($postlist -> have_posts(  )):$postlist -> the_post(  );
-                        ?>
-
-                        <article class="latest-news">
-                            <a href="<?php the_permalink(  ); ?>"><?php the_post_thumbnail('large'); ?></a>
-                            <a href="<?php the_permalink(  ); ?>"><h3><?php the_title() ?></h3></a>
-                            <div class="meta-info">
-                                <p> by <span><?php the_author_posts_link(); ?></span> Categories: <span><?php the_category(' '); ?></span> Tags: <?php the_tags( '', ', ', '' ) ?></p>
-                                <p><span><?php echo get_the_date(); ?></span></p>
-                            </div>
-                            <?php the_excerpt(); ?>
-                        </article>
-
-                        <?php endwhile;
+                            // Content from part/content-latest-news.php
+                            get_template_part( 'part/content', 'latest-news' );
+                           endwhile;
                         wp_reset_postdata(  );
                          else: ?> 
                          <p>Nothing yet to be displayed!</p>
