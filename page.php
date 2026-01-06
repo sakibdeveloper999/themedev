219ab73e54b4bfe56f96e78e3ff6c412d5026ed0
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-06 20:16:48 +0600

    update the page.php and content-page.php

--- page.php
+++ page.php
@@ -12,14 +12,9 @@
                             <div class="page-item">
                           <?php
                             while(have_posts(  )):the_post(  );
-                        ?>
-                        <article>
-                            <header>
-                                <h2><?php the_title() ?></h2>
-                            </header>
-                            <?php the_content(); ?>
-                        </article>
-                        <?php endwhile;
+                          // Content from part/content.php 
+                          get_template_part( 'part/content', 'page' );
+                          endwhile;
                          ?> 
                           </div>
                         </div>
---
+++ part/content-page.php
@@ -0,0 +1,6 @@
+                        <article>
+                            <header>
+                                <h2><?php the_title() ?></h2>
+                            </header>
+                            <?php the_content(); ?>
+                        </article>
