25224fbe6b194e5f308dbcde3b57b77d9e413234
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-06 19:29:59 +0600

    the_archive_title("",""); func add the archive.php file to show Title in Archive template.

--- archive.php
+++ archive.php
@@ -9,6 +9,7 @@
         <div id="content" class="site-content">
             <div id="primary" class="content-area">
                 <main id="main" class="site-main">
+                    <?php the_archive_title( '<h1 class="archive-title">', '</h1>' ) ?>
                         <div class="container">
                             <div class="archive-item">
                           <?php 
