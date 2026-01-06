b1a7f3aceeb9fae2c4ff0c093c14274c3c757ca3
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-06 20:26:12 +0600

    update the single.php and content-single.php

--- single.php
+++ single.php
@@ -7,19 +7,10 @@
         while(have_posts()):
             the_post();
          ?>
-         <article id="post-<?php the_ID(); ?>"  <?php post_class(); ?> > 
-            <header>
-                <h1><?php the_title(); ?></h1>
-                <div class="meta-info">
-                    <p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-                    <p>Categories: <?php the_category(', '); ?></p>
-                    <p>Tags: <?php the_tags( '', ', ', '' ) ?></p>
-                </div>
-            </header>
-            <div class="content">
-                <?php the_content(); ?>
-            </div>
-         </article>
+         <?php 
+            // Content from part/content-single.php
+            get_template_part( 'part/content', 'single' );
+         ?>
          <!--  Pagination -->
              <div class="wpdevs-pagination">
                  <div class="pages next">
