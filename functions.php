0c175689291f5fb25745a95c2e809129755223c0
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-06 19:39:17 +0600

    Add func add_theme_support( 'title-tag' ); to show the page title

--- functions.php
+++ functions.php
@@ -36,6 +36,7 @@ add_action( 'wp_enqueue_scripts', 'themeDev_enqueueing' );
             'flex-width' => true,
             'flex-height' => true,
         ));
+        add_theme_support( 'title-tag' );
     }
     add_action( 'after_setup_theme', 'themeDev_config',0 );
 
