ee7247d95f7eafcd885feb67b011cf7c00825ace
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-14 12:18:08 +0600

    Add Copyright customizer function in customizer.php

---
+++ inc/customizer.php
@@ -0,0 +1,30 @@
+<?php 
+
+function themeDav_customizer($wp_customize){
+    // copyright section
+    $wp_customize -> add_section(
+        'sec_copyright', array(
+            'title'     => 'Copyright Settings',
+            'description'   => 'Customize copyright information',
+        )
+    );
+
+                $wp_customize -> add_setting(
+                    'set_copyright', array(
+                        'type'      => 'theme_mod',
+                        'default' => 'Copyright © 2024 Your Company. All rights reserved.',
+                        'sanitize_callback' => 'sanitize_text_field',
+                    )
+                );
+
+                $wp_customize -> add_control(
+                    'set_copyright', array(
+                        'label' => 'Copyright Information',
+                        'description' => 'Enter your copyright text',
+                        'section'   => 'sec_copyright',
+                        'type'      => 'text',
+                    )
+                );
+}
+
+add_action( 'customize_register', 'themeDav_customizer' );
