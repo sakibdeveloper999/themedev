ad524bccaffdc356c6ebeeb92febc0df20a0d695
sakibdeveloper999 <arjunsakib43@gmail.com>
2026-01-15 01:09:02 +0600

    Add hero section customization options in customizer.php

--- inc/customizer.php
+++ inc/customizer.php
@@ -24,6 +24,112 @@ function themeDav_customizer($wp_customize){
                         'type'      => 'text',
                     )
                 );
-}
+        // Hero Section
+    $wp_customize -> add_section(
+        'sec_hero', array(
+            'title'     => 'Hero Settings',
+            'description'   => 'Customize hero section',
+        )
+    );
+                // Hero Title setting
+                    $wp_customize -> add_setting(
+                    'set_hero_title', array(
+                        'type'      => 'theme_mod',
+                        'default' => 'Please, add some title',
+                        'sanitize_callback' => 'sanitize_text_field',
+                    )
+                );
+                $wp_customize -> add_control(
+                    'set_hero_title', array(
+                        'label' => 'Hero Title',
+                        'description' => 'Enter your hero title',
+                        'section'   => 'sec_hero',
+                        'type'      => 'text',
+                    )
+                );
+
+                //Hero Subtitle setting
+                    $wp_customize -> add_setting(
+                    'set_hero_subtitle', array(
+                        'type'      => 'theme_mod',
+                        'default' => 'Please, add some subtitle',
+                        'sanitize_callback' => 'sanitize_textarea_field',
+                    )
+                );
+                $wp_customize -> add_control(
+                    'set_hero_subtitle', array(
+                        'label' => 'Hero Subtitle',
+                        'description' => 'Enter your hero subtitle',
+                        'section'   => 'sec_hero',
+                        'type'      => 'textarea',
+                    )
+                );
+                //Hero Button Text setting
+                    $wp_customize -> add_setting(
+                    'set_hero_button_text', array(
+                        'type'      => 'theme_mod',
+                        'default' => 'Learn More',
+                        'sanitize_callback' => 'sanitize_text_field',
+                    )
+                );
+                $wp_customize -> add_control(
+                    'set_hero_button_text', array(
+                        'label' => 'Hero Button Text',
+                        'description' => 'Enter your hero button text',
+                        'section'   => 'sec_hero',
+                        'type'      => 'text',
+                    )
+                );
+                // Hero Button LINK setting
+                    $wp_customize -> add_setting(
+                    'set_hero_button_link', array(
+                        'type'      => 'theme_mod',
+                        'default' => '#',
+                        'sanitize_callback' => 'esc_url_raw',
+                    )
+                );
+                $wp_customize -> add_control(
+                    'set_hero_button_link', array(
+                        'label' => 'Hero Button URL',
+                        'description' => 'Enter your hero button link',
+                        'section'   => 'sec_hero',
+                        'type'      => 'url',
+                    )
+                );                
+                // Hero Height setting
+                    $wp_customize -> add_setting(
+                    'set_hero_height', array(
+                        'type'      => 'theme_mod',
+                        'default' => 800,
+                        'sanitize_callback' => 'absint',
+                    )
+                );
+                $wp_customize -> add_control(
+                    'set_hero_height', array(
+                        'label' => 'Hero Height',
+                        'description' => 'Enter your hero height (e.g., 800px)',
+                        'section'   => 'sec_hero',
+                        'type'      => 'number',
+                    )
+                );
+                // Hero Background Image setting  
+                    $wp_customize -> add_setting(
+                    'set_hero_background', array(
+                        'type'      => 'theme_mod',
+                        'sanitize_callback' => 'absint',
+                    )
+                );
+                $wp_customize -> add_control( new WP_Customize_Media_Control(
+                    $wp_customize,
+                    'set_hero_background',
+                    array(
+                        'label'      => 'Hero Background Image',
+                        'description' => 'Upload your hero background image',
+                        'section'    => 'sec_hero',
+                        'mime_type'  => 'image',
+                    )
+                 )
+                );
+}                                       
 
 add_action( 'customize_register', 'themeDav_customizer' );
